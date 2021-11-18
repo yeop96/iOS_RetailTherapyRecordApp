7ad4033e200fad6145ec5db4c5f874efdf5883f0
yeop96 <ekdh6@naver.com>
2021-11-19 04:11:50 +0900

    cost 모델 및 테이블 작성

---
+++ RetailTherapyRecord/Models/CostModel.swift
@@ -0,0 +1,30 @@
+//
+//  CostModel.swift
+//  RetailTherapyRecord
+//
+//  Created by yeop on 2021/11/19.
+//
+
+import Foundation
+import RealmSwift // 10.18.0 exact version
+
+//https://docs.mongodb.com/realm/sdk/ios/quick-start 문서
+
+class CostList: Object {
+    @Persisted var costSubject: String
+    @Persisted var costMoney: Int?
+    @Persisted var costContent: String?
+    @Persisted var costDate = Date()
+    //@Persisted var costEmoji: String
+    @Persisted(primaryKey: true) var _pk: ObjectId
+    
+    convenience init(costSubject: String, costMoney: Int, costContent: String, costDate: Date) { //,costEmoji: String
+        self.init()
+        
+        self.costSubject = costSubject
+        self.costMoney = costMoney
+        self.costContent = costContent
+        self.costDate = costDate
+        //self.costEmoji = costEmoji
+    }
+}
