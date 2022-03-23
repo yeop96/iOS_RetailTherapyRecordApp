b8803ac4e312e13d00caa8643b65fe6440b90f6c
yeop96 <ekdh6@naver.com>
2022-03-23 23:56:28 +0900

    Noto Sans CJK KR 폰트 Extension

--- RetailTherapyRecord/Extensions/UIFont+Extension.swift
+++ RetailTherapyRecord/Extensions/UIFont+Extension.swift
@@ -30,3 +30,44 @@ extension UIFont{
         return UIFont(name: "NanumBaReunHiPi", size: 24)!
     }
 }
+
+//        Noto Sans CJK KR
+
+extension UIFont{
+    var Display1_R20: UIFont {
+        return UIFont(name: "NotoSansCJKKr-Regular", size: 20)!
+    }
+    var Title1_M16: UIFont {
+        return UIFont(name: "NotoSansCJKkr-Medium", size: 16)!
+    }
+    var Title2_R16: UIFont {
+        return UIFont(name: "NotoSansCJKkr-Regular", size: 16)!
+    }
+    var Title3_M14: UIFont {
+        return UIFont(name: "NotoSansCJKkr-Medium", size: 14)!
+    }
+    var Title4_R14: UIFont {
+        return UIFont(name: "NotoSansCJKkr-Regular", size: 14)!
+    }
+    var Title5_M12: UIFont {
+        return UIFont(name: "NotoSansCJKkr-Medium", size: 12)!
+    }
+    var Title6_R12: UIFont {
+        return UIFont(name: "NotoSansCJKkr-Regular", size: 12)!
+    }
+    var Body1_M16: UIFont {
+        return UIFont(name: "NotoSansCJKkr-Medium", size: 16)!
+    }
+    var Body2_R16: UIFont {
+        return UIFont(name: "NotoSansCJKkr-Regular", size: 16)!
+    }
+    var Body3_R14: UIFont {
+        return UIFont(name: "NotoSansCJKkr-Regular", size: 14)!
+    }
+    var Body4_R12: UIFont {
+        return UIFont(name: "NotoSansCJKkr-Regular", size: 12)!
+    }
+    var caption_R10: UIFont {
+        return UIFont(name: "NotoSansCJKkr-Regular", size: 10)!
+    }
+}
