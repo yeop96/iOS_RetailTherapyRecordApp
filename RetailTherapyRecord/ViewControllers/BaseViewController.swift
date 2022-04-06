d7c6553c747a412ed20d84bbe7721ba18785293a
yeop96 <ekdh6@naver.com>
2022-04-06 23:05:18 +0900

    공통 기능을 위한 BaseViewController 구현

---
+++ RetailTherapyRecord/ViewControllers/BaseViewController.swift
@@ -0,0 +1,38 @@
+//
+//  BaseViewController.swift
+//  RetailTherapyRecord
+//
+//  Created by yeop on 2022/04/05.
+//
+
+import UIKit
+import SnapKit
+
+class BaseViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+        setupConstraints()
+    }
+    
+    func configure(){
+    }
+    
+    func setupConstraints(){
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func navigationBarFontSet(){
+        if #available(iOS 15, *) {
+            self.navigationController?.navigationBar.standardAppearance.titleTextAttributes = [.font : UIFont().customFont21]
+            self.navigationController?.navigationBar.scrollEdgeAppearance?.titleTextAttributes = [.font : UIFont().customFont21]
+        } else{
+            self.navigationController?.navigationBar.titleTextAttributes = [.font: UIFont().customFont21]
+        }
+    }
+    
+}
