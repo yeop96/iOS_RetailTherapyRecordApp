22265f4b4827aa47c8bb06fc8dac54a0e9440af8
yeop96 <ekdh6@naver.com>
2022-04-07 23:47:27 +0900

    BaseViewController 적용

--- RetailTherapyRecord/ViewControllers/FontSettingController.swift
+++ RetailTherapyRecord/ViewControllers/FontSettingController.swift
@@ -8,21 +8,22 @@
 
 import UIKit
 
-final class FontSettingViewController: UIViewController{
+final class FontSettingViewController: BaseViewController{
     
     @IBOutlet weak var recordView: UIView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var moneyLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
-    
     @IBOutlet var radioButtons: [UIButton]!
     var indexOfOneAndOnlySelectedBtn = UserData.customUserFont
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func configure() {
         title = "나의 글씨체"
-        self.navigationController?.navigationBar.titleTextAttributes = [.font: UIFont().customFont21]
-        
+        navigationBarFontSet()
         recordView.layer.borderWidth = 1
         recordView.layer.cornerRadius = 8
         recordView.layer.borderColor = UIColor.primary.cgColor
@@ -47,19 +48,12 @@ final class FontSettingViewController: UIViewController{
         
         UserData.customUserFont = indexOfOneAndOnlySelectedBtn
         
-        if #available(iOS 15, *) {
-            self.navigationController?.navigationBar.standardAppearance.titleTextAttributes = [.font : UIFont().customFont21]
-            self.navigationController?.navigationBar.scrollEdgeAppearance?.titleTextAttributes = [.font : UIFont().customFont21]
-        } else{
-            self.navigationController?.navigationBar.titleTextAttributes = [.font: UIFont().customFont21]
-        }
+        navigationBarFontSet()
         titleLabel.font = UIFont().customFont18
         moneyLabel.font = UIFont().customFont17
         descriptionLabel.font = UIFont().customFont17
     }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-    }
+    
     
 }
