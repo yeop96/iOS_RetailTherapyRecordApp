55fc9baa92f43f1964a76ba533d09b80a582d41a
yeop96 <ekdh6@naver.com>
2022-04-18 15:30:01 +0900

    NavigationBar 및 Tabbar TitleTextAttributes 수정

--- RetailTherapyRecord/ViewControllers/BaseViewController.swift
+++ RetailTherapyRecord/ViewControllers/BaseViewController.swift
@@ -31,13 +31,38 @@ class BaseViewController: UIViewController{
     }
     
     func navigationBarFontSet(){
+        let attribute :[NSAttributedString.Key: Any] = [NSAttributedString.Key.font : UIFont().customFont_Navigation]
+        
         if #available(iOS 15, *) {
-            self.navigationController?.navigationBar.standardAppearance.titleTextAttributes = [.font : UIFont().customFont_Navigation]
-            self.navigationController?.navigationBar.scrollEdgeAppearance?.titleTextAttributes = [.font : UIFont().customFont_Navigation]
+            let naviAppearance = UINavigationBarAppearance()
+            naviAppearance.titleTextAttributes = attribute
+            naviAppearance.largeTitleTextAttributes = attribute
+            
+            self.navigationController?.navigationBar.standardAppearance = naviAppearance
+            self.navigationController?.navigationBar.scrollEdgeAppearance = naviAppearance
         } else{
-            self.navigationController?.navigationBar.titleTextAttributes = [.font: UIFont().customFont_Navigation]
+            self.navigationController?.navigationBar.titleTextAttributes = attribute
         }
         
+    }
+    
+    func tabBarFontSet(){
+        let appearance = UITabBarAppearance()
+        let attribute :[NSAttributedString.Key: Any] = [NSAttributedString.Key.font : UIFont().customFont_Navigation]
+        appearance.configureWithOpaqueBackground()
+        appearance.stackedLayoutAppearance.normal.titleTextAttributes = attribute
+        appearance.stackedLayoutAppearance.selected.titleTextAttributes = attribute
+        appearance.inlineLayoutAppearance.normal.titleTextAttributes = attribute
+        appearance.inlineLayoutAppearance.selected.titleTextAttributes = attribute
+        appearance.compactInlineLayoutAppearance.normal.titleTextAttributes = attribute
+        appearance.compactInlineLayoutAppearance.selected.titleTextAttributes = attribute
+        
+        if #available(iOS 15.0, *) {
+            self.tabBarController?.tabBar.standardAppearance = appearance
+            self.tabBarController?.tabBar.scrollEdgeAppearance = appearance
+        } else {
+            self.tabBarController?.tabBar.standardAppearance = appearance
+        }
     }
     
     func tabBarHiddenSet(hidden: Bool){
